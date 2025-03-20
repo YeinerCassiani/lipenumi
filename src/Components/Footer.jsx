2eb8dec68a26d90241a8e91b97151798a46c5605
YeinerCassiani <zeacmlo18@gmail.com>
2025-03-19 23:15:50 -0500

    Update Footer component with new social media links and improve formatting

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,24 +1,42 @@
 import React from "react";
-import { FacebookOutlined, XOutlined, InstagramOutlined, WhatsAppOutlined } from '@ant-design/icons';
+import {
+  FacebookOutlined,
+  XOutlined,
+  InstagramOutlined,
+  WhatsAppOutlined,
+} from "@ant-design/icons";
 
 const Footer = () => {
   return (
     <div style={footerStyle}>
       <div style={footerContentStyle}>
         <p style={footerTextStyle}>
-          &copy; {new Date().getFullYear()} COLEGIO LIPENUMI. All rights reserved.
+          &copy; {new Date().getFullYear()} COLEGIO LIPENUMI. All rights
+          reserved.
         </p>
         <div style={socialIconsStyle}>
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.facebook.com/liceopedagogico.nuevomilenio"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FacebookOutlined style={iconStyle} />
           </a>
           <a href="https://x.com" target="_blank" rel="noopener noreferrer">
             <XOutlined style={iconStyle} />
           </a>
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.instagram.com/lipenumi/?hl=es"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <InstagramOutlined style={iconStyle} />
           </a>
-          <a href="https://wa.me/573003929873" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://wa.me/573003929873"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <WhatsAppOutlined style={iconStyle} />
           </a>
         </div>
